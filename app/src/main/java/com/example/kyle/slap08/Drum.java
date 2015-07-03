9616e089e569545431740d9460f97fd4c53dda6f
kyle <stcalica@gmail.com>
2015-07-02 21:54:37 -0700

    added recording!

--- app/src/main/java/com/example/kyle/slap08/Drum.java
+++ app/src/main/java/com/example/kyle/slap08/Drum.java
@@ -1,18 +1,22 @@
 package com.example.kyle.slap08;
 
-import android.content.Context;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class Drum extends AppCompatActivity {
+    MediaRecorder rec;
+    boolean recording = false;
     boolean loaded =false;
     Button bass;
     Button snare;
@@ -21,8 +25,13 @@ public class Drum extends AppCompatActivity {
     Button clap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+       rec = new MediaRecorder();
+        rec.setAudioSource(MediaRecorder.AudioSource.MIC);
+        rec.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        rec.setOutputFile("track1"); //change to different output files
+        rec.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         final SoundPool sp = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-        final int bassID = sp.load(this, R.raw.bass, 1);
+        final int bassID = sp.load(this, R.raw.deep, 1);
         final int snareID = sp.load(this, R.raw.snare, 1);
         final int kickID = sp.load(this, R.raw.kick, 1);
         final int shakeID = sp.load(this, R.raw.shake, 1);
@@ -97,13 +106,50 @@ public class Drum extends AppCompatActivity {
         return true;
     }
 
+    protected  void recordToggle(){
+        if(recording == true){
+            recording = false;
+            rec.stop();
+            Toast t = Toast.makeText(getApplicationContext(), "Recoding Off", Toast.LENGTH_SHORT);
+            t.show();
+        } else {
+            recording = true;
+            try{
+                rec.prepare();
+            } catch (IOException e){
+                Log.e("Recorder", "prepare() failed");
+            }
+            rec.start();
+            Toast t = Toast.makeText(getApplicationContext(), "Recoding On", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+
+    }
+
+    protected  void playBack(){
+
+        Toast t = Toast.makeText(getApplicationContext(), "Playback", Toast.LENGTH_SHORT);
+        t.show();
+
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch(id){
+            case R.id.action_record:
+                recordToggle();
+                return true;
+            case R.id.action_play:
+                playBack();
+                return true;
 
+        }
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
